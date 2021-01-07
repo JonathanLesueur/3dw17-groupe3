a190ee41d6d3b07b67623f50b70bcc0423a1534f
Jonathan Lesueur <jonathan.lesueur.tic@gmail.com>
2021-01-07 16:11:07 +0100

    main: update base name

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 // New Code
 var monk = require('monk');
-var db = monk('localhost:27017/nodetest1');
+var db = monk('localhost:27017/3dw17');
 
 var indexRouter = require('./routes/index');
 
